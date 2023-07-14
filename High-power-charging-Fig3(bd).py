4f9444e13ba69baaf755d35c748d6a121da79df6
1024Person <239903524@qq.com>
2023-07-14 08:50:26 +0800

    现在任然是把最大值当作充电的能量来看的

--- High-power-charging-Fig3(bd).py
+++ High-power-charging-Fig3(bd).py
@@ -14,16 +14,19 @@ import sys
 
 N_list = np.array([6, 8, 10, 12])
 g_max = 2
-N_g = 100
-N_t = 1000
-g_list = np.linspace(0, g_max, N_g)
+N_g = 50
+g_list = np.linspace(0.1, g_max, N_g)
 
 M = 20
 wc = 1
 wb = 1
 
-t = 80
-t_list = np.linspace(1e-4, t, N_t)
+t_s = 8 # strength coupling
+t_w = 80 # weak coupling
+N_t_s = 1000
+N_t_w = 10000
+t_s_list = np.linspace(0, t_s, N_t_s)
+t_w_list = np.linspace(0, t_w, N_t_w)
 
 
 # define the function used for calculating the energy of quantum batteries
@@ -31,6 +34,11 @@ def energy_dicke(g, N):
     """
     the Dicke model
     """
+    if g<0.1: # weak coupling
+        tlist = t_w_list
+    else: # strength coupling
+        tlist = t_s_list
+
     j = N / 2
     n = 2 * j + 1
 
@@ -50,9 +58,9 @@ def energy_dicke(g, N):
 
     ave_Jz_0 = expect(Jz, psi_0)
 
-    result = mesolve(H, psi_0, t_list, [], [Jz])
+    result = mesolve(H, psi_0, tlist, [], [Jz])
     ave_Jz_t = np.array(result.expect[0])
-    return (max(ave_Jz_t - ave_Jz_0), t_list[ave_Jz_t.argmax()])
+    return (max(ave_Jz_t - ave_Jz_0), tlist[ave_Jz_t.argmax()])
 
 
 # calcuate and show
@@ -64,7 +72,7 @@ for N in N_list:
     for g in g_list:
         d_et = energy_dicke(g, N)
         e_d.append(d_et[0] / N)
-        p_d.append(d_et[0] / d_et[1])
+        p_d.append(d_et[0] / d_et[1] / (N*g*np.sqrt(N)*wc**2))
         print('N={},g={}'.format(N,g))
     e_ds.append(e_d)
     p_ds.append(p_d)
@@ -85,14 +93,14 @@ ax.spines['bottom'].set_linewidth(1.7)
 ax.spines['top'].set_linewidth(1.7)
 ax.spines['right'].set_linewidth(1.7)
 ax.spines['left'].set_linewidth(1.7)
-# plt.legend(fontsize=12)
+# plt.legend(fontsize=8)
 
 # draw Fig 3 (d)
 plt.figure(figsize=(8, 6), dpi=80)
-plt.plot(g_list, p_ds[0], color='black', linestyle='-', label='N = 6')
-plt.plot(g_list, p_ds[1], color='red', linestyle='-.', label='N = 8')
-plt.plot(g_list, p_ds[2], color='blue', linestyle='--', label='N = 10')
-plt.plot(g_list, p_ds[3], color='green', linestyle=':', label='N = 12')
+plt.plot(g_list, p_ds[0], color='black', linestyle='-',  label = 'N = 6' )
+plt.plot(g_list, p_ds[1], color='red',   linestyle='-.', label = 'N = 8' )
+plt.plot(g_list, p_ds[2], color='blue',  linestyle='--', label = 'N = 10')
+plt.plot(g_list, p_ds[3], color='green', linestyle=':',  label = 'N = 12')
 
 ax = plt.gca()
 ax.set_xlabel('$\\bar\\lambda$', fontsize=18, labelpad=1)
