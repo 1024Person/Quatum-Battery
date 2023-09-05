500e1815e5b9dbffde1ce4ed95faf1526c10f9e2
1024Person <239903524@qq.com>
2023-09-05 16:21:38 +0800

    完善了一下，当不穿参数的时候的处理情况

--- tools/draw.py
+++ tools/draw.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-def draw(t,arg,style,fignum,label,ylabel=''):
+def draw(t,arg,style,fignum,ylabel=None,label=None):
     plt.figure(num=fignum)
-    plt.plot(t,arg,style,label =label)
-    plt.ylabel(ylabel)
+    if label:
+        plt.plot(t,arg,style,label =label)
+    else:
+        plt.plot(t,arg,style)
+    if ylabel:
+        plt.ylabel(ylabel)
     plt.xlabel(r'$t/t_{min}$')
     plt.legend()
