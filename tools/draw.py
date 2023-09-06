3e864927e4f17872972987fdc5b927a89ba42225
1024Person <239903524@qq.com>
2023-09-06 15:27:50 +0800

    新增划子图

--- tools/draw.py
+++ tools/draw.py
@@ -1,4 +1,15 @@
 import matplotlib.pyplot as plt
+def draw_sub(t,arg,style,sub_num,ylabel=None,label=None):
+    plt.subplot(sub_num)
+    if label:
+        plt.plot(t,arg,style,label =label)
+    else:
+        plt.plot(t,arg,style)
+    if ylabel:
+        plt.ylabel(ylabel)
+    plt.xlabel(r'$t/t_{min}$')
+    plt.legend()
+
 
 def draw(t,arg,style,fignum,ylabel=None,label=None):
     plt.figure(num=fignum)
