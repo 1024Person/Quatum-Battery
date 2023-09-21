78a39283d33a2332ae34f42458614505c67a0b70
1024Person <239903524@qq.com>
2023-09-21 19:10:41 +0800

    不知道为啥总是不对，先上传上，等会从ipad上看

--- Fig2-Extractable-work-the-role-of-correlations.py.py
+++ Fig2-Extractable-work-the-role-of-correlations.py.py
@@ -2,11 +2,11 @@ from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-tao =1
+tao =2*np.pi
 tn = 1000
 t = np.linspace(0,tao,tn)
 N = 8
-M = 32
+M = 4*N
 g = 1
 hbar = 1
 omega0 = 1
@@ -15,6 +15,7 @@ n = 2*j+1
 alpha = np.sqrt(N)
 sub_sigma = []
 plu_sigma = []
+sz_sigma = []
 # 单位矩阵
 si = qeye(2)
 # 构造湮灭算符和产生算符
@@ -23,6 +24,7 @@ a_dag = a.dag()
 # 构造单粒子的升降算符,在循环中会用到
 sup = jmat(1/2,'+')
 sdown = jmat(1/2,'-')
+sz = jmat(1/2,'z')
 # 构造Fock态
 psi_0 = []
 # 创建含有8个单位矩阵的列表
@@ -39,6 +41,10 @@ for i in range(N):
     op_list[i] = sdown
     temp = tensor(op_list)
     sub_sigma.append(temp)
+    # 构造sigmaz算符
+    op_list[i] = sz
+    temp = tensor(op_list)
+    sz_sigma.append(temp)
     # 重新把第i个元素赋值为单位矩阵，因为下一次循环还需要用到这个列表
     # 如果，不现在把这个元素改回来，下次可能会出错
     op_list[i] = si
@@ -57,15 +63,16 @@ def WE_calc(g):
     参数g代表耦合强度
     """
     # 构造充电器的哈密顿量
-    HA = omega0*a*a_dag
+    HA = omega0*a_dag*a
     # 构造电池的哈密顿量和相互作用哈密顿量
     HB = 0
     H1 = 0
     HB1 = 0
     for i in range(N):
-        HB+=omega0*tensor(sub_sigma[i],qeye(M))*tensor(plu_sigma[i],qeye(M))
+        HB+=omega0*tensor(sz_sigma[i],qeye(M))
         H1+=g*(a*tensor(plu_sigma[i],qeye(M))+a_dag*tensor(sub_sigma[i],qeye(M)))
-        HB1 += omega0*sub_sigma[i]*plu_sigma[i]
+        HB1 += omega0*sz_sigma[i]
+
     # 体系哈密顿量
     H = HA+HB+H1
     EB0 = expect(HB,psi_all)
@@ -75,19 +82,20 @@ def WE_calc(g):
     W = np.ones_like(t)
     for i in range(tn):
         psi = states[i]
-        E[i] = expect(HB,psi)-EB0
         rhoB = ptrace(psi,[0,1,2,3,4,5,6,7])
+        E[i] = np.trace(HB1*rhoB)
         vb1 = np.array(sorted(rhoB.eigenenergies(),reverse=True)) # 降序排列
         vb2 = HB1.eigenenergies()
         m = np.sum(vb1*vb2)
         W[i] = E[i] - m
+        E[i] = E[i] - EB0
         print('i={}'.format(i))
 
-    return E,W
+    return np.abs(E),np.abs(W)
 
 E1,W1 = WE_calc(g=1)
-plt.plot(t,E1,color='black',linestyle='-')
-plt.plot(t,W1,color='red',linestyle='--')
+plt.plot(t*np.sqrt(N)*g,E1/N,color='black',linestyle='-')
+plt.plot(t*np.sqrt(N)*g,W1/N,color='red',linestyle='--')
 
 plt.show()
 
